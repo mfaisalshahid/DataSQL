05759665afd2653b4dbbce2c4716e90207fcd206
mfaisalshahid <56493157+mfaisalshahid@users.noreply.github.com>
2021-08-05 21:04:54 -0400

    Update game-play-analysis-ii.sql

--- game-play-analysis-ii.sql
+++ game-play-analysis-ii.sql
@@ -13,3 +13,9 @@ FROM (
     from activity
 ) tmp
 where tmp.rank = 1
+
+
+/////
+select distinct player_id
+, first_value(device_id) over (partition by player_id order by event_date) device_id
+from activity
