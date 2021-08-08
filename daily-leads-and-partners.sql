f1c898cf7d0521b0f9b4d61bc436c249af7f7dda
mfaisalshahid <56493157+mfaisalshahid@users.noreply.github.com>
2021-08-07 23:03:28 -0400

    Create daily-leads-and-partners.sql
    Table: DailySales
    
    +-------------+---------+
    | Column Name | Type    |
    +-------------+---------+
    | date_id     | date    |
    | make_name   | varchar |
    | lead_id     | int     |
    | partner_id  | int     |
    +-------------+---------+
    This table does not have a primary key.
    This table contains the date and the name of the product sold and the IDs of the lead and partner it was sold to.
    The name consists of only lowercase English letters.
     
    
    Write an SQL query that will, for each date_id and make_name, return the number of distinct lead_id's and distinct partner_id's.
    
    Return the result table in any order.
    
    The query result format is in the following example:
    
     
    
    DailySales table:
    +-----------+-----------+---------+------------+
    | date_id   | make_name | lead_id | partner_id |
    +-----------+-----------+---------+------------+
    | 2020-12-8 | toyota    | 0       | 1          |
    | 2020-12-8 | toyota    | 1       | 0          |
    | 2020-12-8 | toyota    | 1       | 2          |
    | 2020-12-7 | toyota    | 0       | 2          |
    | 2020-12-7 | toyota    | 0       | 1          |
    | 2020-12-8 | honda     | 1       | 2          |
    | 2020-12-8 | honda     | 2       | 1          |
    | 2020-12-7 | honda     | 0       | 1          |
    | 2020-12-7 | honda     | 1       | 2          |
    | 2020-12-7 | honda     | 2       | 1          |
    +-----------+-----------+---------+------------+
    Result table:
    +-----------+-----------+--------------+-----------------+
    | date_id   | make_name | unique_leads | unique_partners |
    +-----------+-----------+--------------+-----------------+
    | 2020-12-8 | toyota    | 2            | 3               |
    | 2020-12-7 | toyota    | 1            | 2               |
    | 2020-12-8 | honda     | 2            | 2               |
    | 2020-12-7 | honda     | 3            | 2               |
    +-----------+-----------+--------------+-----------------+
    For 2020-12-8, toyota gets leads = [0, 1] and partners = [0, 1, 2] while honda gets leads = [1, 2] and partners = [1, 2].
    For 2020-12-7, toyota gets leads = [0] and partners = [1, 2] while honda gets leads = [0, 1, 2] and partners = [1, 2].

---
+++ daily-leads-and-partners.sql
@@ -0,0 +1,13 @@
+SELECT date_id, make_name, COUNT(DISTINCT lead_id) AS unique_leads, COUNT(DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY 1, 2
+
+///////
+
+SELECT        date_id, 
+			  make_name, 
+			  COUNT(DISTINCT lead_id) as unique_leads, 
+			  COUNT( DISTINCT partner_id) AS unique_partners
+FROM DailySales
+GROUP BY date_id, make_name
+ORDER BY make_name DESC, date_id DESC
